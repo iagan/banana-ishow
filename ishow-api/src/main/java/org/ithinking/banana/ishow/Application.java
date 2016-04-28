fae504c28d2d904bbe9f3510ed24bd20ba13de23
fucj <fucj@hyxt.com>
2016-04-28 12:46:42 +0800

    完善

--- ishow-api/src/main/java/org/ithinking/banana/ishow/Application.java
+++ ishow-api/src/main/java/org/ithinking/banana/ishow/Application.java
@@ -1,13 +1,19 @@
 package org.ithinking.banana.ishow;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author agan
  * @since 2016-04-27
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "org.ithinking.banana")
+@MapperScan(basePackages = {"org.ithinking.banana.ishow.mapper"})
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
