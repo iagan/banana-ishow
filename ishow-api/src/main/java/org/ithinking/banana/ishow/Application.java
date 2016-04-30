f271ed990b7318aed3cf750fd06c4e5bd1d4e808
fucj <fucj@hyxt.com>
2016-04-30 14:49:50 +0800

    完善

--- ishow-api/src/main/java/org/ithinking/banana/ishow/Application.java
+++ ishow-api/src/main/java/org/ithinking/banana/ishow/Application.java
@@ -17,6 +17,14 @@ import java.util.Arrays;
 //@MapperScan(basePackages = {"org.ithinking.banana.ishow.mapper"})
 @EnableAutoConfiguration
 public class Application {
+
+
+    /**
+     * WebApplicationInitializer
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         /**
