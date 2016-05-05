503e3da6d9945b2e16a89df35413a4416783c089
fucj <fucj@hyxt.com>
2016-05-05 15:10:38 +0800

    完善

--- ishow-api/src/main/java/org/ithinking/banana/ishow/api/controller/ArticleController.java
+++ ishow-api/src/main/java/org/ithinking/banana/ishow/api/controller/ArticleController.java
@@ -4,6 +4,7 @@ import org.ithinking.banana.ishow.model.entity.Article;
 import org.ithinking.banana.ishow.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,8 +24,35 @@ public class ArticleController {
         return articleList;
     }
 
-    public List<Article> getArticlePage(Integer page){
-
-        return null;
+    /**
+     * 分页获取文章列表
+     *
+     * @param pageNum
+     * @return
+     */
+    @RequestMapping("/articlePage")
+    public List<Article> getArticlePage(Integer pageNum){
+        return articleService.getArticlePage(pageNum == null ? 1 : pageNum);
     }
+
+    /**
+     * 奖赏一分钱
+     */
+    public void clickPenny(@RequestParam("authorId")Long authorId, @RequestParam("articleId")Long articleId){
+        // 1.从账号中减钱
+        // 2.加入钱
+        // 3.
+    }
+
+    // 银
+    public void clickSilver(){
+
+    }
+
+    // 金
+    public void clickGold(){
+
+    }
+
+
 }
